5eb341e5c32d54040b951fbfdb464223cf79e362
Aron <acrisostomo1@cps.edu>
2025-02-10 12:12:27 -0600

    2/10

--- ArraysLab1/main.swift
+++ ArraysLab1/main.swift
@@ -170,8 +170,8 @@ print("***PROBLEM 2.0 : FITNESS TRACKING")
  Using arrays of type String, create at least THREE lists (arrays), one for walking challenges, and one for running challenges, and one for lifting challenges - in that order. Each should have at least THREE challenges or how many times a week it's done and should be initialized using an array. Feel free to create more lists for different activities.
  */
 //Print all three of the arrays to make sure it outputs in the console.
-
-
+var possibleChallenges : [String] = ["Walking Challenges", "running challenges", "alisthenics challenges", " weightlifting challenges"]
+print(possibleChallenges)
 
 
 print("\n")
